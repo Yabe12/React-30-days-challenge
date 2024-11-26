743a67b5e737c0f13c9a0d65688ca11672a57c31
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-26 16:45:22 +0300

    100% complete of the level 1 and level 2

--- 01_Day_javascript_Refresher/Solutions/Exercisees-2/Level1/level1.js
+++ 01_Day_javascript_Refresher/Solutions/Exercisees-2/Level1/level1.js
@@ -5,23 +5,51 @@
 
 // Enter your age:15
 // You are left with 3 years to drive.
-let age = prompt('Enter your age?') 
-if (age>=30) {
-  console.log('You are old enough to drive.')
-}else{
-  console.log('You are left with 3 years to drive')
-}
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('Enter your age? ', (age) => {
+
+//   console.log(`You entered: ${age}`); 
+//   if (age>=30) {
+//     console.log('You are old enough to drive.')
+//   }else{
+//     console.log('You are left with 3 years to drive')
+//   }
+//   rl.close();
+// });
 // Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 
 // Enter your age: 30
 // You are 5 years older than me.
 
-let myAge = 30
-let yourAge = prompt('Enter your age:')
-let difference = myAge - yourAge
-if (difference>0) {
-  console.log('You are ' + difference +'years older than me.')
-}else{
-  console.log('You are ' + Math.abs(difference) +'years younger than me.')
-}
 
+
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter your age? ', (age) => {
+  let myAge = 30
+  let difference = myAge - age
+  if (difference>0) {
+    console.log('You are ' + difference +'years older than me.')
+  }else{
+    console.log('You are ' + Math.abs(difference) +'years younger than me.')
+  }
+  console.log(`You entered: ${age}`); 
+  if (age>=30) {
+    console.log('You are old enough to drive.')
+  }else{
+    console.log('You are left with 3 years to drive')
+  }
+  rl.close();
+});
