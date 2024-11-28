ade7e23d8a01f9aca9812dde890f24b9457bfbbc
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-28 09:13:18 +0300

    100% complete of the first task of exerce 3

--- 01_Day_javascript_Refresher/ObjectExercises/Level3.js
+++ 01_Day_javascript_Refresher/ObjectExercises/Level3.js
@@ -1,35 +1,35 @@
 // Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
-const personAccount = {
-  firstName: "John",
-  lastName: "Doe",
-  incomes: [
-    { description: "Salary", amount: 5000 },
-    { description: "Bonus", amount: 2000 },
-  ],
-  expenses: [
-    { description: "Rent", amount: 1000 },
-    { description: "Groceries", amount: 500 },
-  ],
-  totalIncome() {
-    return this.incomes.reduce((total, income) => total + income.amount, 0);
-  },
+// const personAccount = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   incomes: [
+//     { description: "Salary", amount: 5000 },
+//     { description: "Bonus", amount: 2000 },
+//   ],
+//   expenses: [
+//     { description: "Rent", amount: 1000 },
+//     { description: "Groceries", amount: 500 },
+//   ],
+//   totalIncome() {
+//     return this.incomes.reduce((total, income) => total + income.amount, 0);
+//   },
 
-  totalExpense(){
-    return this.expenses.reduce((total, expense) => total + expense.amount, 0);
-  },
-  accountInfo(){
-    return `Name: ${this.firstName} ${this.lastName}`;
-  },
-  addIncome(){
-    const income = { description: "New Income", amount: 1000 };
-    this.incomes.push(income);
-  },
-  addExpense (){
-    const expense = { description: "New Expense", amount: 500 };
-    this.expenses.push(expense);
-  },
-  accountBalance(){
-    return this.totalIncome() - this.totalExpense();
-  }
+//   totalExpense(){
+//     return this.expenses.reduce((total, expense) => total + expense.amount, 0);
+//   },
+//   accountInfo(){
+//     return `Name: ${this.firstName} ${this.lastName}`;
+//   },
+//   addIncome(){
+//     const income = { description: "New Income", amount: 1000 };
+//     this.incomes.push(income);
+//   },
+//   addExpense (){
+//     const expense = { description: "New Expense", amount: 500 };
+//     this.expenses.push(expense);
+//   },
+//   accountBalance(){
+//     return this.totalIncome() - this.totalExpense();
+//   }
  
-}
+// }
