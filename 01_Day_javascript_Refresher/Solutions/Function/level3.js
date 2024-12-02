1545361e8570712c0e885ea74f6a689fa86fb989
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-02 09:28:32 +0300

    50% complete the level3   challenge in function

---
+++ 01_Day_javascript_Refresher/Solutions/Function/level3.js
@@ -0,0 +1,58 @@
+// Declare a function name userIdGeneratedByUser. It doesn’t take any parameter but it takes two inputs using prompt(). One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('Enter your characters: ', (characters) => {
+//   rl.question('Enter your num: ', (numInput) => {
+//     const num = parseInt(numInput, 10); 
+   
+//     let id = '';
+//     for (let i = 0; i < num; i++) {
+//       id += characters.charAt(Math.floor(Math.random() * characters.length));
+//     }
+
+//     console.log('Your random ID is:', id);
+
+    
+//     rl.close();
+//   });
+// });
+//******************************************************
+// Write a function generateColors which can generate any number of hexa or rgb colors.
+//*****************************************************?//
+// ****
+// Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
+// // *****************************************************
+// const shuffleArray =(arr) => {
+// let shuffledArray =[];
+
+// }
+// const arr=[1,92,87];
+// console.log(shuffleArray(arr));
+
+// Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
+const factorial=()=>{
+  let fact=1;
+  for(let i=1;i<=5;i++){
+    fact=fact*i;
+  }
+  return fact;
+}
+
+console.log(factorial());
+
+// Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+
+const isEmpty=(arr) => {
+  if(arr.length===0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(isEmpty([1,2,3]));
+// Write a function called average, it takes an array parameter and returns the average of the items. Check if all the array items are number types. If not give return reasonable feedback.
