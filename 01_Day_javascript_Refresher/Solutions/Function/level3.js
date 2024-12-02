013d4b158094fc28658831f30b8f08e29fdcfa92
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-02 17:13:34 +0300

    calculate the avarage of the item

--- 01_Day_javascript_Refresher/Solutions/Function/level3.js
+++ 01_Day_javascript_Refresher/Solutions/Function/level3.js
@@ -145,3 +145,9 @@ const isEmpty=(arr) => {
 
 console.log(isEmpty([1,2,3]));
 // Write a function called average, it takes an array parameter and returns the average of the items. Check if all the array items are number types. If not give return reasonable feedback.
+ const average=(arr) => {
+  if(!arr.every(Number.isFinite)){
+    return "All items in the array should be numbers";
+  }
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+}
