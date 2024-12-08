a8b533c7226af598a0b85fc8e498eaf316def5c7
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-08 22:49:57 +0300

    complete the first task

---
+++ 15-30_Day_javascript_Refresher/Solutions/Json/level1.js
@@ -0,0 +1,13 @@
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+// Change skills array to JSON using JSON.stringify()
+const skill = JSON.stringify(student, ['skills']);
+console.log((skill));
