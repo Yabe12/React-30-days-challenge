234a76f499d090247285c7f7dbacee13fde8bd5d
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-08 23:09:09 +0300

    complete the level 2 task

---
+++ 15-30_Day_javascript_Refresher/Solutions/Json/level2.js
@@ -0,0 +1,13 @@
+const skills = ['HTML', 'CSS', 'JS', 'React','Node', 'Python']
+let age = 250;
+let isMarried = true
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+// Stringify the students object with only firstName, lastName and skills properties
+const students = JSON.stringify(student,['firstName','lastName','skills']);
+console.log(students);
